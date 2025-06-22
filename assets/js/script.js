d9e077845d610c67045c7876aaa1bb429f9d4fe5
Buck Roxy <william.27@free.fr>
2025-06-22 20:50:08 +0200

    naze

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,37 +7,25 @@ function afficherNombre(nombre) {
     }
 
     document.getElementById('affichage').innerText += nombre
+
 }
 
 function effacerNombre() {
+
     document.getElementById('affichage').innerText = 0
 
     if (affichage.innerText >= 0) {
         document.getElementById('clear').innerText = ''
     }
+
 }
 
-function executerOperation(nombre, operation) {
-    document.getElementById('affichage').innerText = 0
+function calculer(nombre) {
+    let calculer = document.getElementById('calculer')
 
-    return operation(nombre, nombre)
-
-    function additionner(nombre) {
-        document.getElementById('additionner').innerText = ''
-        return nombre + nombre
+    if (calculer.innerText == nombre) {
+        document.getElementById(affichage).innerText = ''
     }
 
-    function soustraction(nombre) {
-        document.getElementById('soustraction').innerText = ''
-        return nombre + nombre
-    }
-
-    function multiplication(nombre) {
-        return nombre * nombre
-    }
-
-    function division(nombre) {
-        return nombre / nombre
-    }
-
+    document.getElementById('affichage').innerText += nombre
 }
