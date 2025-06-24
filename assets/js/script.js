7b8891a97fc1564744431334640f658356e78eb3
William <william.27@free.fr>
2025-06-24 09:15:34 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,72 @@
+let affichage = "nb1"
+
 function afficherNombre(nombre) {
 
-    let affichage = document.getElementById('affichage')
+    document.getElementById(affichage).innerText += nombre
 
     if (affichage.innerText == 0) {
-        document.getElementById('affichage').innerText = ''
+        document.getElementById(affichage).innerText = ""
     }
-
-    document.getElementById('affichage').innerText += nombre
-
 }
 
 function effacerNombre() {
 
-    document.getElementById('affichage').innerText = 0
+    document.getElementById("nb1").innerText = 0
+    document.getElementById("operande").innerText = ""
+    document.getElementById("nb2").innerText = ""
+    document.getElementById("result").innerText = ""
+    affichage = 'nb1'
 
-    if (affichage.innerText >= 0) {
-        document.getElementById('clear').innerText = ''
-    }
 
 }
 
-function calculer(nombre) {
-    let calculer = document.getElementById('calculer')
+function addOperande(operande) {
+    affichage = "nb2"
+    document.getElementById("operande").innerText = operande
 
-    if (calculer.innerText == nombre) {
-        document.getElementById(affichage).innerText = ''
+}
+
+function divideNumber(nb1, nb2) {
+    return nb1 / nb2
+}
+
+function multiplyNumber(nb1, nb2) {
+    return nb1 * nb2
+}
+
+function addNumber(nb1, nb2) {
+    return nb1 + nb2
+}
+
+function minusNumber(nb1, nb2) {
+    return nb1 - nb2
+}
+
+function result() {
+    let nb1 = Number(document.getElementById("nb1").innerText)
+    let nb2 = Number(document.getElementById("nb2").innerText)
+    let operande = (document.getElementById('operande').innerText)
+
+    let total = 0
+
+
+    switch (operande) {
+        case "+":
+            total = addNumber(nb1, nb2)
+            break
+        case "-": 
+            total = minusNumber(nb1, nb2)
+            break
+
+        case "*": 
+            total = multiplyNumber(nb1, nb2)
+            break
+
+        case "/": 
+            total = divideNumber(nb1, nb2)
+            break
+        default:
+            console.log("erreur")
     }
-
-    document.getElementById('affichage').innerText += nombre
+    document.getElementById("result").innerText = total
 }
